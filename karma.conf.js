39b772716762047c40bd5d6f9aa65e7d43a86fb9
Burlak Ilia <burlakilia@bk.ru>
2019-05-31 13:38:43 +0300

    Доработал ввод студенческого запуска тестов

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration
 // Generated on Sun Nov 26 2017 12:48:45 GMT+0000 (GMT)
 
+const { TRAVIS } = process.env;
+
 module.exports = function(config) {
   config.set({
     
@@ -34,7 +36,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['json'], // progress
+    reporters: [TRAVIS ? 'json' : 'dots'], // progress
     
     jsonReporter: {
       stdout: true,
