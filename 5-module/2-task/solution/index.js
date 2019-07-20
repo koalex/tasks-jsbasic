9a390de133b6fe81efcb4e32ee915b5c8ee1866f
Burlak Ilia <burlakilia@bk.ru>
2019-07-20 12:59:50 +0300

    Поправил опечатки

--- 5-module/2-task/solution/index.js
+++ 5-module/2-task/solution/index.js
@@ -22,13 +22,32 @@ function SortableTable(items) {
    */
   this.el = document.createElement('table');
 
-  var list = rows.map(function(i) {return (
-    '<tr><td>'+i.name+'</td><td>'+i.age+'</td><td>'+i.salary+'</td><td>'+i.city+'</td></tr>'
-  )}).join('');
+  let thead = document.createElement('thead');
+  let tbody = document.createElement('tbody');
 
-//var final =  '<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead><tbody>'+list+'</tbody>'
+  thead.innerHTML = `
+         <tr>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Salary</td>
+            <td>City</td>
+         </tr>
+    `;
 
-  this.el.innerHTML = '<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead><tbody>'+list+'</tbody>'
+  this.el.appendChild(thead);
+  this.el.appendChild(tbody);
+
+  function render() {
+    tbody.innerHTML = items.map(item => {
+      let row = '';
+
+      for (let key in item) {
+        row += `<td>${item[key]}</td>`;
+      }
+
+      return `<tr>${row}</tr>`;
+    }).join('');
+  }
 
   /**
    * Метод выполняет сортировку таблицы
@@ -37,31 +56,21 @@ function SortableTable(items) {
    */
   this.sort = function (column, desc = false) {
 
-//console.log(column)
-    function column_name(item) {
-      if (item === 0) return 'name';
-      if (item === 2) return 'salary';
-    }
+    items = items.sort((a, b) => {
+      let keyA = Object.keys(a)[column];
+      let keyB = Object.keys(b)[column];
 
-    let  col = column_name(column)
-    // console.log(col)
+      if (desc) {
+        return a[keyA] < b[keyB] ? 1 : -1;
+      } else {
+        return a[keyA] > b[keyB] ? 1 : -1;
+      }
 
-    rows.sort(function(a,b){
-      if (a[col] > b[col]) return 1;
-      if (a[col] < b[col]) return -1;
-    })
-//console.log(rows)
+    });
 
-    var list = rows.map(function(i) {return (
-      '<tr><td>'+i.name+'</td><td>'+i.age+'</td><td>'+i.salary+'</td><td>'+i.city+'</td></tr>'
-    )}).join('')
-
-    var final =  '<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead><tbody>'+list+'</tbody>'
-    this.el.innerHTML = final
-//document.querySelector('.result.table').appendChild(final)
-
-//console.log(final)
-//console.log(document.querySelector('.result').firstElementChild.innerHTML)
+    render();
   };
+
+  render();
 }
 
--- 5-module/2-task/src/task.test.js
+++ 5-module/2-task/src/task.test.js
@@ -1,124 +1,124 @@
-describe("8-module-1-task", function() {
+describe("5-module-1-task", function() {
 
-    it ('проверям что при создании таблицы содержит корректный head', function () {
-        let table = new SortableTable([{
-            name: 'Ilia',
-            age: 25,
-            salary: '1000',
-            city: 'Petrozavodsk'
-        }]);
+  it ('проверям что при создании таблицы содержит корректный head', function () {
+    let table = new SortableTable([{
+      name: 'Ilia',
+      age: 25,
+      salary: '1000',
+      city: 'Petrozavods'
+    }]);
 
-        expect(table.el.querySelectorAll('thead').length).toEqual(1);
-        expect(table.el.querySelectorAll('thead td').length).toEqual(4);
-    });
+    expect(table.el.querySelectorAll('thead').length).toEqual(1);
+    expect(table.el.querySelectorAll('thead td').length).toEqual(4);
+  });
 
-    it (`проверяем, что при создании таблица создает элементы в том порядке
+  it (`проверяем, что при создании таблица создает элементы в том порядке
     в котором они перечислены в передаваемом массиве`, function () {
 
-        let table = new SortableTable([
-            {
-                name: 'Artem',
-                age: 30,
-                salary: 10000,
-                city: 'London'
-            },
-            {
-                name: 'Vasiliy',
-                age: 170,
-                salary: 34,
-                city: 'Moscow'
-            },
-            {
-                name: 'Ilia',
-                age: 25,
-                salary: 1000,
-                city: 'Petrozavods'
-            }
-        ]);
+    let table = new SortableTable([
+      {
+        name: 'Artem',
+        age: 30,
+        salary: 10000,
+        city: 'London'
+      },
+      {
+        name: 'Vasiliy',
+        age: 170,
+        salary: 34,
+        city: 'Moscow'
+      },
+      {
+        name: 'Ilia',
+        age: 25,
+        salary: 1000,
+        city: 'Petrozavods'
+      }
+    ]);
 
-        let rows = table.el.querySelectorAll('tbody tr');
+    let rows = table.el.querySelectorAll('tbody tr');
 
-        expect(rows.length).toEqual(3);
-        expect(rows[0].cells[0].innerText).toEqual('Artem');
-        expect(rows[1].cells[0].innerText).toEqual('Vasiliy');
-        expect(rows[2].cells[0].innerText).toEqual('Ilia');
+    expect(rows.length).toEqual(3);
+    expect(rows[0].cells[0].innerText).toEqual('Artem');
+    expect(rows[1].cells[0].innerText).toEqual('Vasiliy');
+    expect(rows[2].cells[0].innerText).toEqual('Ilia');
 
-    });
+  });
 
-    it ('провяерм сортировку имени по алфавитному порядку', function () {
-        let table = new SortableTable([
-            {
-                name: 'Artem',
-                age: 30,
-                salary: 10000,
-                city: 'London'
-            },
-            {
-                name: 'Ilia',
-                age: 25,
-                salary: 1000,
-                city: 'Petrozavods'
-            }
-        ]);
+  it ('провяерм сортировку имени по алфавитному порядку', function () {
+    let table = new SortableTable([
+      {
+        name: 'Artem',
+        age: 30,
+        salary: 10000,
+        city: 'London'
+      },
+      {
+        name: 'Ilia',
+        age: 25,
+        salary: 1000,
+        city: 'Petrozavods'
+      }
+    ]);
 
-        table.sort(0);
+    table.sort(0);
 
-        let rows = table.el.querySelectorAll('tbody tr');
+    let rows = table.el.querySelectorAll('tbody tr');
 
-        expect(rows.length).toEqual(2);
-        expect(rows[0].cells[0].innerText).toEqual('Artem');
-        expect(rows[1].cells[0].innerText).toEqual('Ilia');
-    });
+    expect(rows.length).toEqual(2);
+    expect(rows[0].cells[0].innerText).toEqual('Artem');
+    expect(rows[1].cells[0].innerText).toEqual('Ilia');
+  });
 
 
-    it ('провяерм сортировку имени в обратном порядке', function () {
-        let table = new SortableTable([
-            {
-                name: 'Artem',
-                age: 30,
-                salary: 10000,
-                city: 'London'
-            },
-            {
-                name: 'Ilia',
-                age: 25,
-                salary: 1000,
-                city: 'Petrozavods'
-            }
-        ]);
+  it ('провяерм сортировку имени в обратном порядке', function () {
+    let table = new SortableTable([
+      {
+        name: 'Artem',
+        age: 30,
+        salary: 10000,
+        city: 'London'
+      },
+      {
+        name: 'Ilia',
+        age: 25,
+        salary: 1000,
+        city: 'Petrozavods'
+      }
+    ]);
 
-        table.sort(0, true);
+    table.sort(0, true);
 
-        let rows = table.el.querySelectorAll('tbody tr');
+    let rows = table.el.querySelectorAll('tbody tr');
 
-        expect(rows.length).toEqual(2);
-        expect(rows[0].cells[0].innerText).toEqual('Ilia');
-        expect(rows[1].cells[0].innerText).toEqual('Artem');
-    });
+    expect(rows.length).toEqual(2);
+    expect(rows[0].cells[0].innerText).toEqual('Ilia');
+    expect(rows[1].cells[0].innerText).toEqual('Artem');
+  });
 
-    it ('провяерм сортировку зарпалты по возрастарнию', function () {
-        let table = new SortableTable([
-            {
-                name: 'Artem',
-                age: 30,
-                salary: 10000,
-                city: 'London'
-            },
-            {
-                name: 'Ilia',
-                age: 25,
-                salary: 100,
-                city: 'Petrozavods'
-            }
-        ]);
+  it ('провяерм сортировку зарпалты по возрастарнию', function () {
+    let table = new SortableTable([
+      {
+        name: 'Artem',
+        age: 30,
+        salary: 10000,
+        city: 'London'
+      },
+      {
+        name: 'Ilia',
+        age: 25,
+        salary: 100,
+        city: 'Petrozavods'
+      }
+    ]);
 
-        table.sort(2);
+    table.sort(2);
 
-        let rows = table.el.querySelectorAll('tbody tr');
+    let rows = table.el.querySelectorAll('tbody tr');
 
-        expect(rows.length).toEqual(2);
-        expect(rows[0].cells[0].innerText).toEqual('Ilia');
-        expect(rows[1].cells[0].innerText).toEqual('Artem');
-    });
+    expect(rows.length).toEqual(2);
+    expect(rows[0].cells[0].innerText).toEqual('Ilia');
+    expect(rows[1].cells[0].innerText).toEqual('Artem');
+  });
 
 });
