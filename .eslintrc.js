2e8b92eb9c94aec909c9c9cf12a61e0e3a28e54d
Stanislav <s.dolgachov@gmail.com>
2019-10-11 11:18:08 +0300

    Упростил линт конфиг (#5)
    * Убрал поддержку правил Airbnb
    
    * Добавил базовые правила для eslintrc
    
    * Добавил правила ES2015 в линт конфиг
    
    * Добавил npm script для автоисправления ошибок линта

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,6 @@ module.exports = {
     es6: true,
     mocha: true
   },
-  extends: [
-    'airbnb-base',
-  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
@@ -16,14 +13,58 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'no-unused-vars': 'off',
-    'no-undef': 'off',
-    'no-console': 'off',
-    'no-restricted-syntax': 'off',
-    'guard-for-in': 'off',
-    'no-param-reassign': 'off',
-    'class-methods-use-this': 'off',
-    'max-len': ["error", { "code": 120 }],
-    'no-alert': 'off'
+    'no-debugger': 'error',
+    'no-dupe-keys': 'error',
+    'no-dupe-args': 'error',
+    'no-duplicate-case': 'error',
+    'no-empty': 'error',
+    'no-extra-semi': 'error',
+    'no-func-assign': 'error',
+    'no-obj-calls': 'error',
+    'no-sparse-arrays': 'error',
+    'no-unreachable': 'error',
+    'valid-typeof': 'warn',
+    'no-multi-spaces': 'warn',
+    'no-with': 'warn',
+    'curly': 'warn',
+    'no-unused-expressions': 'warn',
+    'no-shadow-restricted-names': 'error',
+    'no-undefined': 'off',
+    'camelcase': 'error',
+
+    'indent': ['error', 2, {
+      // continuation indent
+      MemberExpression: 'off',
+    }],
+
+    'array-bracket-spacing': ['error', 'never'],
+    'comma-spacing': 'error',
+    'comma-style': 'error',
+    'computed-property-spacing': 'error',
+    'func-call-spacing': 'error',
+    'key-spacing': 'error',
+    'keyword-spacing': 'error',
+    'linebreak-style': ['error', 'unix'], // we can check this in git as well
+    'new-cap': 'error',
+    'no-mixed-spaces-and-tabs': 'error',
+    'one-var': ['error', {
+      var: 'never',
+      let: 'never',
+      const: 'never'
+    }],
+    'quotes': ['error', 'single', {allowTemplateLiterals: true}],
+    'semi': 'error',
+    'space-in-parens': ['error', 'never'],
+    'space-before-blocks': 'error',
+    'unicode-bom': 'warn',
+    'new-parens': 'error',
+    'no-nested-ternary': 'error',
+    'space-infix-ops': 'error',
+    'space-unary-ops': ['error', {words: true, nonwords: false}],
+
+    // ES2015
+    'constructor-super': 'error',
+    'no-this-before-super': 'error',
+    'no-var': 'error',
   },
 };
