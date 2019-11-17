c9ee29267500fcc7e9880711d9f415e3c772e9ad
Stanislav Dolgachov <s.dolgachov@gmail.com>
2019-11-17 22:34:50 +0200

    Обновил решение компоненты ProductList

--- 7-module/1-task/src/index.js
+++ 7-module/1-task/src/index.js
@@ -7,50 +7,20 @@ class ProductList {
     return `/assets/data/${category}.json`;
   }
 
-  show({title, id }) {
+  show({ id }) {
     const urlForFetch = this.getUrl(id);
 
     // Здесь код загрузки продуктов через fetch
     fetch(urlForFetch)
-      .then(resp => {console.log('!!!', resp); return resp.json(); })
-      .then(data => this.render(data))
-      .catch(err => console.error('show error', err));
+      .then((resp) => resp.json())
+      .then((data) => this.render(data))
+      .catch((err) => console.error('show error', err));
   }
 
   render({ title, items }) {
-    console.log(title, items);
-
-    const list = items.map(item => (`
-      <div class="col-md-6 col-lg-4 mb-4">
-        <div class="card">
-            <div class="card-img-wrap">
-                <img class="card-img-top" src="${item.imageUrl}" alt="${item.title}"/>
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">${item.title}</h5>
-                <div class="rate">
-                    <i class="icon-star checked"></i>
-                    <i class="icon-star checked"></i>
-                    <i class="icon-star checked"></i>
-                    <i class="icon-star checked"></i>
-                    <i class="icon-star checked"></i>
-                    <span class="rate-amount ml-2">24</span>
-                </div>
-                ${ item.oldPrice ? `
-                    <p class="card-text price-text discount">
-                        <strong>${item.price}</strong>
-                        <small class="ml-2">${item.oldPrice}</small>
-                    </p>
-                ` : `
-                  <p class="card-text price-text">
-                      <strong>${item.price}</strong>
-                  </p>
-                `}
-            </div>
-        </div>
-      </div>
-    `)).join('');
-
+    const list = items
+      .map((product) => this._generateProduct(product))
+      .join('');
 
     this.el.innerHTML = `
       <div>
@@ -60,6 +30,82 @@ class ProductList {
         </div>
       </div>
     `;
+  }
+
+  _generateProduct(product) {
+    const allStarsHTML = this._generateStars(product.rating);
+    const ratingReviewsHTML = this._generateReviewsAmount(product.rating);
+    const priceHTML = this._generatePrice(product);
+
+    return `
+    <div class="col-md-6 col-lg-4 mb-4">
+        <div class="card">
+            <div class="card-img-wrap">
+              <img class="card-img-top" src="${product.imageUrl}" alt="${product.title}"/>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">${product.title}</h5>
+              <div class="rate">
+                  ${allStarsHTML}
+                  ${ratingReviewsHTML}
+              </div>
+              ${priceHTML}
+          </div>
+      </div>
+    </div>
+`;
+  }
+
+  _generateReviewsAmount(productRating) {
+    if (productRating) {
+      return `
+        <span class="rate-amount ml-2">${productRating.reviewsAmount}</span>
+      `;
+    }
+
+    return '';
+  }
+
+  _generateStars(productRating) {
+    let ratingStarsCount;
+    if (productRating) {
+      ratingStarsCount = productRating.stars;
+    } else {
+      ratingStarsCount = 0;
+    }
+
+    let allStarsHTML = '';
+    let maximumStarsCount = 5;
+    let star = '<i class="icon-star"></i>';
+    let starChecked = '<i class="icon-star checked"></i>';
+
+    for (let i = 0; i < maximumStarsCount; i++) {
+      if (ratingStarsCount === 0) {
+        allStarsHTML += star;
+      } else {
+        allStarsHTML += starChecked;
+        ratingStarsCount--;
+      }
+    }
+
+    return allStarsHTML;
+  }
+
+  _generatePrice(product) {
+    if (product.oldPrice) {
+      return `
+        <p class="card-text price-text discount">
+            <strong>${product.price}</strong>
+            <small class="ml-2">${product.oldPrice}</small>
+        </p>
+      `;
+    } else {
+      return `
+        <p class="card-text price-text">
+            <strong>${product.price}</strong>
+        </p>
+      `;
+    }
   }
 }
 
